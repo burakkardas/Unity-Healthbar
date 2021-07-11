efae8eb3e9f46c6779b1bdcf02c45a2f5323b55f
Burak Kardaş <krdsbrk@gmail.com>
2021-07-11 16:30:48 +0300

    Unity 2D Healthbar

---
+++ Healthbar/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Image _healtBar;
+    [SerializeField] private float _currentHealt;
+    [SerializeField] private float _maxHealt;
+    private Player _player;
+    void Start()
+    {
+        _player = GameObject.FindObjectOfType<Player>();
+        _healtBar = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        _currentHealt = _player._playerHealt;
+        Debug.Log(_currentHealt);
+        _healtBar.fillAmount = _currentHealt / _maxHealt;
+        
+    }
+}
---
+++ Healthbar/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{   
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _moveX;
+    public float _playerHealt = 100;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        _moveX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(
+            _moveX * _moveSpeed,
+            rb.velocity.y
+        );
+
+        if(_playerHealt <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Enemy") {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage() {
+        _playerHealt -= 20;
+    }
+}
